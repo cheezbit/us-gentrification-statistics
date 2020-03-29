72a1c0267a67b2ad7fcba788f72c09c00d47da65
jasonTelanoff <jason.telanoff@gmail.com>
2020-03-29 07:24:13 -0700

    Fixed some stuff, won't run

--- src/index.js
+++ src/index.js
@@ -56,3 +56,22 @@ d3.json(geoURL).then(us => {
         .attr('class', 'state')
         .attr('d', path);
 })
+
+//Get stuff
+var firebaseConfig = {
+  apiKey: "AIzaSyC1kpIjnsgf3A4INGLK7rOHE5JlVQx6sCw",
+  authDomain: "genetrificationmapping.firebaseapp.com",
+  databaseURL: "https://genetrificationmapping.firebaseio.com",
+  projectId: "genetrificationmapping",
+  storageBucket: "genetrificationmapping.appspot.com",
+  messagingSenderId: "39572194143",
+  appId: "1:39572194143:web:6f4c71803dac4d3443ecea",
+  measurementId: "G-measurement-id",
+};
+
+
+const db = firebase.firestore();
+
+var housingCostCounty = db.collection("housingCostCountyWide");
+
+console.log(housingCostCounty["Abbeville County"]["Metro"])
