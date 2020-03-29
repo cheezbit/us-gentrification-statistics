cb4ea3b156e0bce1d70f4496a3f8f791b4bce1fc
jasonTelanoff <jason.telanoff@gmail.com>
2020-03-29 08:02:24 -0700

    tried

--- src/index.js
+++ src/index.js
@@ -58,16 +58,6 @@ d3.json(geoURL).then(us => {
 })
 
 //Get stuff
-var firebaseConfig = {
-  apiKey: "AIzaSyC1kpIjnsgf3A4INGLK7rOHE5JlVQx6sCw",
-  authDomain: "genetrificationmapping.firebaseapp.com",
-  databaseURL: "https://genetrificationmapping.firebaseio.com",
-  projectId: "genetrificationmapping",
-  storageBucket: "genetrificationmapping.appspot.com",
-  messagingSenderId: "39572194143",
-  appId: "1:39572194143:web:6f4c71803dac4d3443ecea",
-  measurementId: "G-measurement-id",
-};
 
 
 const db = firebase.firestore();
